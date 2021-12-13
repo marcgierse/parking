4a2ee9d48d51a59e491c1e70860d6887bb5a58fa
marcgierse <marc@gierse.org>
2021-12-13 13:51:09 +0100

    Update settings.py

--- parking/settings.py
+++ parking/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['parkplatz.gierse.tech', 'localhost', '81.169.218.230', '127.0.0.1']
+ALLOWED_HOSTS = ['parkplatz.gierse.tech', 'localhost', '127.0.0.1']
 
 
 LOGGING = {
